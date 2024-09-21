51e27dd71fbf4d01b4c959ffb1c163a2adff2396
Diego Scarpati <diegoscarpati13@gmail.com>
2024-09-21 18:09:04 +1000

    Modified eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint, { plugin } from "typescript-eslint";
 
 
 export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {extends: ["eslint:recommended", "plugin:prettier/recommended"]},
+  {plugins: ["prettier"]},
+  {rules: {"prettier/prettier": "error"}},
 ];
