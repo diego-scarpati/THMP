a3781dd79aeb25aea21ec48038fd229bf8b860e7
Diego Scarpati <91996215+diego-scarpati@users.noreply.github.com>
2025-07-03 17:58:18 +1000

    fix foreign key creation

--- src/services/jobKeyword.services.ts
+++ src/services/jobKeyword.services.ts
@@ -1,8 +1,16 @@
 import JobKeyword from "../models/JobKeyword.js";
+import { Transaction } from "sequelize";
 
-export const createJobKeyword = async (jobId: string, keywordId: number) => {
+export const createJobKeyword = async (
+  jobId: string,
+  keywordId: number,
+  transaction?: Transaction
+) => {
   try {
-    const jobKeyword = await JobKeyword.create({ jobId, keywordId });
+    const jobKeyword = await JobKeyword.create(
+      { jobId, keywordId },
+      { transaction }
+    );
     return jobKeyword;
   } catch (error) {
     console.error("Error creating JobKeyword:", error);
